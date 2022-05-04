dfa2e61d2168bf85422bf7cf326165ec36f9d976
Bruno-Jang <sosdnrud@gmail.com>
2022-05-04 16:10:21 +0900

    style : #7 restaurants CRUD 관련 주석 추가

--- apps/restaurants/urls.py
+++ apps/restaurants/urls.py
@@ -4,6 +4,6 @@ from .views import RestaurantListAPIView, RestaurantDetailAPIView
 
 
 urlpatterns = [
-    path('', RestaurantListAPIView.as_view()),
-    path('/<int:pk>', RestaurantDetailAPIView.as_view()),
+    path('', RestaurantListAPIView.as_view()),  # Restaurant 생성 및 전체 리스트 조회
+    path('/<int:pk>', RestaurantDetailAPIView.as_view()),   # Restaurant 상세 정보 조회, 수정 및 삭제
 ]
--- apps/restaurants/views.py
+++ apps/restaurants/views.py
@@ -4,6 +4,7 @@ from .models import Restaurant
 from .serializer import RestaurantSerializer
 
 
+# Restaurnat 데이터 생성 및 전체 리스트 조회 API
 class RestaurantListAPIView(generics.ListCreateAPIView):
     '''
     Assignee : 장우경
@@ -13,6 +14,7 @@ class RestaurantListAPIView(generics.ListCreateAPIView):
     serializer_class = RestaurantSerializer
 
 
+# Restaurnat 상세 정보 조회, 수정(업데이트), 삭제 API
 class RestaurantDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     '''
     Assignee : 장우경
