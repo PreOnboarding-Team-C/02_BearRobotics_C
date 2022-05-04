2206418ddedbf94700e5bbdd3fab01180eedc0be
castela0119 <castela0119@gmail.com>
2022-05-04 12:08:39 +0900

    feat : #6-gitignore에 migrations 추가

--- apps/restaurants/admin.py
+++ apps/restaurants/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from apps.restaurants.models import Group, Restaurant
 # Register your models here.
+
+admin.site.register([Group, Restaurant])
+
--- apps/sales/admin.py
+++ apps/sales/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Pos
 # Register your models here.
+
+admin.site.register(Pos)
